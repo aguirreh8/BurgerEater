b35eb2ae1a0323af698cd3496c30fe5d6371eeef
unknown <aguirreh8@gmail.com>
2018-03-05 18:06:52 -0500

    added handlebars/css layout and style, started some handlebars script

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name) VALUES ("Whopper");
+INSERT INTO burgers(burger_name) VALUES ("Baconator");
+INSERT INTO burgers(burger_name) VALUES ("Big Mac");
--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+const routes = require("./controllers/burgerControllers.js")
 
-app.use(express.static("public"));
+app.use(express.static('/public/'));
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(routes);
 
 app.listen(PORT, function() {
 	console.log(`Listening to port ${PORT}`);
